8faa09603043742cd3d96ec79e12334febcf30e6
Sharon A <sharon.alajajian@gmail.com>
2018-10-12 18:04:21 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,17 @@ class Song
         @name = name
     end
 
+    def artist_name
+        if self.artist == nil
+            return nil
+        else
+            self.artist.name
+        end
+    end
+
     def self.new_by_filename(some_filename)
         n = Song.new(some_filename.split(/\ \-\ /)[1])
+        n.artist_name
         n
     end
 end
