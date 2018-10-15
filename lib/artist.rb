94098250cd5d2b55d01fdeec88485c1e2599f21e
Austin  Burke <aburk3@gmail.com>
2018-10-15 17:12:22 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,7 +24,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist_name)
-    self.find(name) ? self.find(name) : self.create(name)
+    self.find(artist_name) ? self.find(artist_name) : self.create(artist_name)
   end
 
   def self.find(name)
@@ -39,7 +39,7 @@ class Artist
   end
 
   def print_songs
-     songs.each {|song| puts song.title}
-   end
+    songs.each {|song| puts song.name}
+  end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,23 @@
 class Song
-  attr_accessor :title
+  attr_accessor :name, :artist
 
-  def initialize(title)
-    @title = title
+  def initialize(name)
+    @name = name
   end
   
+  def self.new_by_filename(file_name)
+    artist, song = file_name.split(" - ")
+    # binding.pry
+    added_song = self.new(song)
+    added_song.artist_name = artist
+    added_song
+  end
+  
+  # Creating writer method
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    # self = #<Song: ..... @artist=#<Artist: ..... @name="Michael Jackson", @songs=[]>, @name="Black or White">
+    # binding.pry
+    artist.add_song(self)
+  end
 end
