9d247b4cc22715f5b80c54540b13d1a9be955705
Cookie McCormick <muntananuchat@gmail.com>
2018-11-05 23:42:31 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.find_or_create_by_name(name)
+    @@all.find {|artist| artist.name == name} || self.new(name).save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+    self
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+end
