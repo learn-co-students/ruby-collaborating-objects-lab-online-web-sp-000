cf23260b7f11c7bd5c0206aa8219c2594bbcb720
Sean Langton <slangton1@gmail.com>
2018-11-09 19:34:54 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,14 +2,20 @@ require 'pry'
 
 class MP3Importer
 
-attr_accessor :path
+attr_accessor :path, :files
 
-  def inititalize(path)
+  def initialize(path)
     @path = path
+    @files = []
   end
 
-  def path
-    @path
+  def files
+    @files = Dir.entries(@path).reject{|entry| entry =~ /^.{1,2}$/}
+    @files.each {|filename| filename.chomp(".mp3")}
+  end
+
+  def import
+    files.each{|file| Song.new_by_filename(file)}
   end
 
 end
---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require 'pry'
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+  end
+
+  def self.new_by_filename(filename)
+    artist, song = filename.split(" - ")
+#    binding.pry
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+    binding.pry
+  end
+
+end
