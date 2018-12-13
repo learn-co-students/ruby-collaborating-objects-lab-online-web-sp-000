395783fbcb9d1acdbb3eff40431717a69c1c2308
Meisuleen Kased <meisuleen@gmail.com>
2018-12-13 17:06:19 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
+require 'pry'
 class Artist
   attr_accessor :name, :songs, :artist
   @@all = []
+
   def initialize(name)
     @name = name
     @songs = []
@@ -8,7 +10,7 @@ class Artist
 
   def add_song(song)
     @songs << song
-    # song.artist = self
+    song.artist = self
   end
 
   def save
@@ -19,26 +21,26 @@ class Artist
      @@all
    end
 
-
-  #  def self.create_by_name(name)     Try to squeeze all these steps into the #find_or_create_by_name
-  #    song = self.new
-  #    song.save
-  #    song.name = name
-  #    song
-  #  end
-   #
-  #  def self.find_by_name(name)
-  #    self.all.find {|song| song.name == name}
-  #  end
-
-   def self.find_or_create_by_name(name)
-     if song = self.find_by_name(name)
-       song
-     else
-       self.create_by_name(name)
-     end
+   def self.find(name)
+     self.all.find {|artist| artist.name == name}
    end
 
+   def self.create(name)
+     artist_name = Artist.new(name)
+     artist_name.save
+     artist_name
+   end
 
+   def self.find_or_create_by_name(name)
+     if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+   end
+
+   def print_songs
+     puts self.songs.collect {|songs| songs.name}
+   end
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -3,11 +3,14 @@ class Song
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
-  def self.new_by_filename(name)
-    @songs = Song.new_by_filename(name)
+  def self.new_by_filename(file_name)
+    song_data = file_name.split(" - ")
+    song_name = song_data[1]
+    new_song = self.new(song_name)
+    artist = new_song.artist
+    new_song
   end
 
 
