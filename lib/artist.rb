9957df0174f9e340eb0da936d1b85385c7f75d8e
karen ercoli <karenercoli@gmail.com>
2019-01-01 17:10:01 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+class Artist
+attr_accessor :name
+@@all = []
+
+def initialize(name)
+  @name = name
+  @songs = []
+  end
+
+  def add_song(song)
+    @songs<<song
+  end
+
+  def songs
+  @songs
+end
+
+def self.all
+  @@all
+end
+
+def save
+  @@all<<self
+end
+
+def self.find_or_create_by_name
+  if name == nil
+  artist_name == name
+end
+end
+
+end
