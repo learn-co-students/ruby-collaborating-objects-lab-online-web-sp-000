bcb8fe0e97bae2748cddbb302ec2a9d08c1bf83a
karen ercoli <karenercoli@gmail.com>
2019-01-02 20:44:48 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,26 @@
 class Artist
-attr_accessor :name
-@@all = []
-
-def initialize(name)
-  @name = name
-  @songs = []
+  attr_accessor :name, :songs
+   @@all = []
+   def initialize(name)
+    @name = name
+    @songs = []
   end
-
-  def add_song(song)
-    @songs<<song
+   def self.find_or_create_by_name(name)
+    @@all.find {|artist| artist.name == name} || self.new(name).save
   end
-
-  def songs
-  @songs
-end
-
-def self.all
-  @@all
-end
-
-def save
-  @@all<<self
-end
-
-def self.find_or_create_by_name
-  if name == nil
-  artist_name == name
-end
-end
-
-end
+   def self.all
+    @@all
+  end
+   def add_song(song)
+    @songs << song
+  end
+   def save
+    @@all << self
+    self
+  end
+   def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
+end 
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,13 +1,19 @@
 class MP3Importer
 
   attr_accessor :path
+  files = []
 
   def initialize(path)
     @path = path
   end
-  def files
+
+def files
+    Dir.chdir(@path) do | path |
+    Dir.glob("*.mp3")
   end
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+end
+
+  def import
+    files.each{ |filename| Song.new_by_filename(filename) }
   end
 end
