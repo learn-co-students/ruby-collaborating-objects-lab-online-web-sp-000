ce1d5705a0c5c9feb43ab28aaab43b8a48567925
Diana Badas <diana.badas@gmail.com>
2019-01-24 20:48:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -12,10 +12,10 @@ class Song
 
 
   def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(name)
+    artist, song = file.chomp(".mp3").split(" - ")
+    song = self.new(song)
     song.name = song_info[1]
-    song.artist = song_info[0]
+    song.artist_name = artist
     song
   end
 end
