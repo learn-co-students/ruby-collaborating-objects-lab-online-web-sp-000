4bb679acec250d9562d9df5a9d1f272ba95ee6ad
Jim Hough <teachlife@outlook.com>
2019-05-03 20:10:13 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,20 +1,21 @@
+require 'pry'
 class MP3Importer
   attr_accessor :path, :filenames
   def initialize (path)
     @path = path
   end
   def files
-    mp3files = File.join("**", "*.mp3")
-    path = Dir.glob(mp3files)
+    mp3files = File.join("**", "/*.mp3")
+    with_file_paths = Dir.glob(mp3files)
     @filenames = []
-    path.each do |path_name|
+    with_file_paths.each do |path_name|
       path_array = path_name.split('/')
       @filenames << path_array[-1]
     end
     @filenames
   end
   def import
-    @filenames.each do |file|
+    self.files.each do |file|
       Song.new_by_filename(file)
     end
   end
