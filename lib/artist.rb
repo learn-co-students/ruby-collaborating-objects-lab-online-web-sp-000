c8daed3174b03b8f0271ac92cf18baccaa20d5cd
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-06 15:54:05 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,6 +23,10 @@ class Artist
     self
   end
 
+  def self.all
+    @@all
+  end
+
   def self.find_or_create_by_name(name)
     self.all.detect{|artist| artist.name==name} || Artist.new(name).save
   end
