2564a7ea15f32db983ffd7fd3204ecb66db46c26
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-14 19:15:43 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(filepath)
+    @path = filepath
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
+  end
+
+  def import
+   files.each {|song| Song.new_by_filename(song)}
+ end
+
+end
