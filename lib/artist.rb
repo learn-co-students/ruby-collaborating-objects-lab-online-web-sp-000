b16eeb9a05d8eea16862467d6f25ec20fc4349c3
Ismail Jalloh <ijalloh21@gmail.com>
2019-06-21 04:03:56 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,9 +21,27 @@ class Artist
     @songs << song
   end
   
-  def save
-    @@all << self
+  def self.all
     @@all
   end
   
+  def save
+    @@all << self
+  end
+
+  def self.find_or_create_by_name(d_name)
+    if self.all.find{|person| person.name == d_name}
+      name
+    else
+      name = self.new(d_name)
+      name
+    end
+  end
+  
+  def print_songs
+    self.all.each do|person| 
+      puts "#{person.name}"
+    end
+  end
+    
 end
