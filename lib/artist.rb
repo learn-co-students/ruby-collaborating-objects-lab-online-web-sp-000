54caa94f7aa0422bc6c53caa69e664eb59e428e5
Shawn <shawntennity@gmail.com>
2019-07-01 17:11:22 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+require 'pry'
+require_relative "../lib/song.rb"
+require_relative "../lib/mp3_importer.rb"
+class Artist
+
+  attr_accessor :name, :songs
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(name)
+    @songs << name
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    artist_exists = self.all.find{ |artist| artist.name == name }
+    artist_exists ? artist_exists :  Artist.new(name)
+  end
+# binding.pry
+end
