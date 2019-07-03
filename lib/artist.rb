8d4410996ca81f279905bd799fd2b2d45316b2e0
Connor Cowan <ccowan003@comcast.net>
2019-07-03 05:23:59 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,29 @@
 class Artist
-  attr_accessor :name
-
+  attr_accessor :name, :songs         #name/song attributes
+  @@all = []                  #Class var set to an array
 
   def initialize(name)
-    @name = name
+    @name = name              #instance var set at instantiation
+    @songs = []
   end
 
-  def name=
+  def self.all
+    return @all
+  end
 
+  def save
+    @@all << self                    #adds the artist instance to the @@all class variable
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.find_or_create_by_name
+
+  end
+
+  def print_songs
+    songs.each {|song| puts song}
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,11 @@
 class Song
   attr_accessor :name, :artist
+
   def initialize(name)
     @name = name
   end
 
+  def self.new_by_filename
+    
+  end
 end
