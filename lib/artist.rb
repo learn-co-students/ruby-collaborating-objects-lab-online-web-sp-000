6779918f9f40526b9f04b30a0d0a86e5d464ca29
Connor Cowan <ccowan003@comcast.net>
2019-07-03 21:25:54 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,14 @@
 class Artist
   attr_accessor :name, :songs         #name/song attributes
-  @@all = []                  #Class var set to an array
+  @@all = []                          #Class var set to an array
 
   def initialize(name)
-    @name = name              #instance var set at instantiation
+    @name = name                      #instance var set at instantiation
     @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
   end
 
   def self.all
@@ -12,18 +16,22 @@ class Artist
   end
 
   def save
-    @@all << self                    #adds the artist instance to the @@all class variable
+    @@all << self                      #adds the artist instance to the @@all class variable
   end
 
-  def add_song(song)
-    @songs << song
+  def self.find_or_create_by_name(name)       #name parameter
+    self.find(name) ? self.find(name) : self.create(name)       #Ternary operator to maintain uniqueness
   end
 
-  def self.find_or_create_by_name
+  def self.find(name)
+    self.all.find {|artist| artist.name == name}
+  end
 
+  def self.create(name)
+    self.new(name).tap {|artist| artist.save}
   end
 
   def print_songs
-    songs.each {|song| puts song}
+    songs.each {|song| puts song.name}
   end
 end
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,16 @@
 class MP3Importer
+  attr_accessor :path
 
-
+  def initialize(path)
+    @path = path            #initialize accepts a file path to parse mp3 files from
+  end
+  #loads all the mp3 files in the path directory
+  def files
+    @file_array = Dir.entries(@path).select{|file| file.include?(".mp3")}
+  #normalizes the filename to just the mp3 filename with no path
+  end
+  #import files into a library by creating (instantiating) new Songs from a filename!
+  def import
+    files.each{|filename| Song.new_by_filename(filename)}
+  end
 end
