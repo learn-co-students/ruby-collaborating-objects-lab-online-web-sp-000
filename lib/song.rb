8ee4bcde89e2a57723ea260904fbc6e9514cc763
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-15 11:13:48 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -7,17 +7,16 @@ def initialize(name)
   @name = name
 end
 
-
-  def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+def self.new_by_filename(file_name)
+  artist, song_name = file_name.split(" - ")
+  new_song = self.new(song_name)
+  new_song.artist_name= artist
+  new_song
   end
 
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    self.artist.add_song(self)
-  end
-  
+def artist_name=(name)
+   self.artist = Artist.find_or_create_by_name(name)
+   self.artist.add_song(self)
+end
+ 
 end
