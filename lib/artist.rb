25cf07487cb47ef0c9a88dafb758adcd5a69587d
Chris Cheong <chrisxcheong@gmail.com>
2019-07-31 05:15:33 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,38 @@
-class Artist 
+
+class Artist
   @@all = []
-  @@passer = {}
-  attr_accessor :name 
+  attr_accessor :name, :songs
 
   def initialize(name)
-    @name = name  
-    @songs = []
-    @@all << self
-    @@passer[name] = self
-  end 
-
-  def self.create_artist(name)
-    artist = Artist.new(name)
-    artist
+    @name = name
+    save
   end
-    
-  def add_song(song)
-    @songs << song 
-    song.artist =  self 
-  end 
-
-  def self.find_or_create_by_name(name)
-    @@passer.keys.include?(name) ? @@passer[name] : Artist.create_artist(name)
-  end 
-
-  def print_songs 
-    @songs.each { |song| puts song.name } 
-  end
-
-  def songs
-    @songs
-  end
-
-  def save
-    
-  end 
 
   def self.all
     @@all
-  end 
-end 
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    songs.each {|song| puts song.name}
+  end
+end
