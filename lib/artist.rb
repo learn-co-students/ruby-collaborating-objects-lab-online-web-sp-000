bb10ae5ea79fb54c83b719ad1c58eb559751a621
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-07 23:20:35 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,19 +8,28 @@ class Artist
     save
   end
 
-  def save
-    @@all << self
-  end
-
   def self.all
     @@all
   end
 
+  def save
+    @@all << self
+  end
+
   def add_song(song)
-    @song << song
     song.artist = self
   end
 
+  def find_or_create_by_name
+    self.find_by_name(name) || Song.self.create_by_filename(filename)
+  end
+
+
+  end
+
+  def self.find_by_name(name)
+    @@.find{ |song| song.name == name}
+  end
 
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,30 @@
 class Song
-  attr_accessor :name, :artist, :title
+  attr_accessor :name, :artist
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    save
   end
 
-  def self.new_by_filename(filemname)
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @all
+  end
+
+  def self.new_by_filename(filename)
     song = self.new
-    song.title = filename.split( " - ")[0]
+    song.name = filename.split( " - ")[0]
+    song.artist = filename.split(" - ") [1].chomp
     song
+  end
+
+  def artist_name
+    artist = Artist.new(song.artist)
   end
 
 end
