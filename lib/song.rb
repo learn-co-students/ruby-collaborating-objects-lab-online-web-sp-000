0d90a20851fed96a45c08d57997fe0228178a799
Panela Leung <leungpanela@gmail.com>
2019-09-12 00:23:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,32 @@
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :name,
 
-  @@all
+  @@all=[]
 
-  def initiatize(name, artist)
-    @title = title
+  def initiatize()
+    @name = name
+    @@all << self
   end
 
-  
+  def self.all(name)
+    song.all
+  end
+
+  def self.new_by_filename (songdata)
+    song = self.new
+    song.title = songdata.split(" - ")[1]
+    #song.artist = self
+    song
+  end
+
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
+  end
+
 
 
 end
