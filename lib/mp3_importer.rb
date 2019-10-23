a4fa2c88e32361194bdc9f5e80ba09cff25b9f00
Anthony Tilelli <anthony@tilelli.me>
2019-10-23 03:17:29 +0000

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,4 +1,13 @@
-def MP3Importer
+class MP3Importer
+  attr_accessor :path, :size
   def initialize(file_path)
+    @path = file_path
+    @size = -1
   end
+  
+  def files
+    binding.pry
+    Dir[@path]
+  end
+
 end
