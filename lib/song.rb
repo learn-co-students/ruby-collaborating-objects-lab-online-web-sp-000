02b43f1ea82f34cd5f8a4902936c189f696f05e4
Otto Cedeno <otto@ottostacos.com>
2019-12-10 18:06:53 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
   def self.new_by_filename(filename)
     song_data = filename.split(" - ")
     new_song = Song.new(song_data[1])
-    new_song.artist = Artist.new(song_data[0])
+    new_song.artist_name = song_data[0]
     new_song
   end
 
@@ -29,10 +29,4 @@ class Song
   end
 end
 
-hit = Song.new("Hit the Lights")
-
-Artist.new("Iron Maiden")
-Artist.new("Metallica")
-
-hit.artist_name=("Metallica")
 # binding.pry
