45f42598751205a117b0ef9e3bf7631f07a632c0
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-09 16:02:08 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,6 +6,7 @@ class Artist
 
   def initialize(name)
     @name = name
+    # @songs = []
     @@all << self
   end
 
@@ -16,20 +17,25 @@ class Artist
 
   # instance method receives an instance of a Song and associates it belongs to this Artist instance.
   def add_song(song)
-    song.artist = self
+    # @songs << song
+    Song.name = self
   end
 
   # This instance method returns an Array of all songs that belong to this Artist instance.
   # This method should get all existing Song instances from Song and select only the ones that are associated with this Artist instance.
   def songs
-    @@all
+    Song.all
   end
 
   # This class method should take the name that is passed in (a string), and do one of two things.
   # Find the artist instance that has that name or create one if it doesn't exist.
   # Either way, the return value of the method will be an instance of an artist with the name attribute filled out.
   def self.find_or_create_by_name(name)
-
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
   end
 
   # instance methods outputs the names of all songs associated with this Artist instance.
--- lib/song.rb
+++ lib/song.rb
@@ -13,16 +13,26 @@ class Song
     @@all
   end
 
-  # parse a filename to find the song name and artist name
-  # then, create a new song instance using the string we gathered from the filename
-  # also associate that new song with an artist. Use the artist attribute accessor to assign this
   # return the new song instance
-  def self.new_by_filename
-
+  # file_name = 'Michael Jackson - Black or White - pop.mp3'
+  def self.new_by_filename(file_name)
+    # parse a filename to find the song name and artist name
+    title = file_name.split(" - ")[1]
+    musician = file_name.split(" - ")[0]
+    # create a new song instance using the string we gathered from the filename
+      # self.new(title)
+    # associate that new song with an artist. Use the artist attribute accessor to assign this
+    @name = self.new(title)
   end
 
-  def artist_name
-
+  # accepts an artist's name,
+  # finds or creates an Artist instance and assigns it to the Song's artist attribute
+  def artist_name=(name)
+    if (self.artist.nil?)
+      self.artist = Artist.new(name)
+    else
+      self.artist.name = name
+    end
   end
 
 end
