19655b5d5670023c14256486a7952964532407cf
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-10 02:00:21 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,16 +29,16 @@ class Artist
   # Find the artist instance that has that name or create one if it doesn't exist.
   # Either way, the return value of the method will be an instance of an artist with the name attribute filled out.
   def self.find_or_create_by_name(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
+    if found_artist = @@all.find {|artist| artist.name == name}
+      found_artist
     else
-      self.artist.name = name
+      artist = Artist.new(name)
     end
   end
 
   # instance methods outputs the names of all songs associated with this Artist instance.
   def print_songs
-    self.artist
+    self.songs.each { |song| puts song.name }
   end
 
 end
