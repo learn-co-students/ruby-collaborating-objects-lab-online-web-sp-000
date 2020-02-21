0c404bf40f86e834f378e401cde3d75d7f55e325
Hendrick Salomon <hsalomon86@gmail.com>
2020-02-21 14:43:59 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
 
   def initialize(name)
     @name = name
-    @songs = []
+    @@all << self
 
   end
 
@@ -14,7 +14,7 @@ class Artist
   end
 
   def add_song(song)
-    @songs << song
+    song.artist = self
   end
 
   def artist_name
