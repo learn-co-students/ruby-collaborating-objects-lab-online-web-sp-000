099a9ff1f457b89029ab6abb5a85a89bb0f3d150
Mark Stabler <mark@markstabler.com>
2020-03-19 00:31:59 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -14,12 +14,12 @@ class Song
 
   def self.new_by_filename(file_name)
       file = file_name.split(" - ")
-      artist_from_file = file[0]
-      name_from_file = file[1]
+      artist = file[0]
+      name = file[1]
       #genre = file[2].chomp(".mp3")
 
-      new_song = self.new(name_from_file)
-      new_song.artist_name=(artist_from_file)
+      new_song = self.new(name)
+      new_song.artist_name=(artist)
       new_song
   end
 
