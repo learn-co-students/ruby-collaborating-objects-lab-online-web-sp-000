f405b698a59be400771567a25ddb76624c1ff13a
Monica Gerard <mlgerardvla@gmail.com>
2020-03-22 04:21:57 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    name = filename.split(" - ")[1]
+    song = self.new(name)
+
+  end
+end
