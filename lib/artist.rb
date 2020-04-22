e85329cc8bf772cedfacd9d59c9b491dcb52461c
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-22 16:49:40 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+  attr_accessor :name
+  @@all
+  def initialize(name)
+    @name = name
+    @all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    song = 
+  end
+  
+end
