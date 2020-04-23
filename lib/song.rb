7d2a6ac0209520f37c568c50cfcfb216f0f8a055
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-23 20:56:18 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -13,12 +13,12 @@ class Song
   def self.new_by_filename(filename)
     array = filename.split(" - ")
     song = self.new(array[1])
-    artist_name=(array[0])
+    song.artist_name=(array[0])
     song
   end
   
   def artist_name=(name)
-    if self.artist == nil
+    if (self.artist.nil?)
       self.artist = Artist.new(name)
     else
       self.artist.name = name
