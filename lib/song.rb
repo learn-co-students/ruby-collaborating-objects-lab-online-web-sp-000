ca5823cb629901321b4cbc0310df12dff4b9b2c9
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-24 04:02:40 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ class Song
   end
   
   def artist_name=(name)
-    Artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
-  
 end
