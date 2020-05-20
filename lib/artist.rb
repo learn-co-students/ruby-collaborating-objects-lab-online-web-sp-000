1beebd13c44036784972487238a1314fa48b854d
Michael Bade <mbade314@gmail.com>
2020-05-20 19:37:33 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
+    self.all.detect {|artist| artist.name == name} || Artist.new(name)
   end
 
   def print_songs
--- lib/song.rb
+++ lib/song.rb
@@ -22,9 +22,17 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    song_info = file.chomp(".mp3").split(" - ")
+    song_info = filename.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
     song.artist_name = song_info[0]
-    Artist.add_song(song)
+    song
   end
+
+  #
+  # def self.new_by_filename(file)
+  #   song_info = file.chomp(".mp3").split(" - ")
+  #   song = Song.new(song_info[1])
+  #   song.artist_name = song_info[0]
+  #   song
+  # end
 end
