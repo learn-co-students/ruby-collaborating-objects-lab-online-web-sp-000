1f412b49780f51fe1eeba054a67ef8f6dad93d81
ting682 <tchung682@gmail.com>
2020-06-11 10:30:06 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,13 +20,14 @@ class Artist
         #binding.pry
     end
     def songs
-        #binding.pry
-        Song.all.select {|song| 
+        
+        artist_songs = Song.all.select {|song| 
             if song.artist == nil
             else
                 song.artist == self
             end
-        }
+        }#.each {|song| puts song.name}
+        #binding.pry
     end
     def self.find_or_create_by_name(name)
         
@@ -38,12 +39,12 @@ class Artist
         end
     end
     def print_songs
-        binding.pry
-        puts Song.all.select {|song| 
+        #binding.pry
+        Song.all.select {|song| 
             if song.artist == nil
             else
                 song.artist == self
             end
-        }
+        }.each {|song| puts song.name}
     end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,12 @@ class Song
         @@all
     end
     def self.new_by_filename(file)
-
+        #binding.pry
+        newFile = file.split(".mp3")[0].split(" - ")
+        newSong = Song.new(newFile[1])
+        newArtist = Artist.find_or_create_by_name(newFile[0])
+        newSong.artist = newArtist
+        newSong
     end
     def artist_name=(artist)
         newArtist = Artist.find_or_create_by_name(artist)
