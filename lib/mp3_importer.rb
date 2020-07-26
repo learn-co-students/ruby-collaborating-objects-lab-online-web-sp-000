22817f94328b3a8a1ba4da6c51967989007c1a07
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-07-26 02:45:07 +0000

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,21 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    files = []
+    Dir["#{@path}/*.mp3"].each do |filename|
+      files << filename.split("/")[-1]
+    end
+    files
+  end
+
+  def import
+    self.files.each do  |filename|
+      Song.new_by_filename(filename)
+    end
+  end
+end
