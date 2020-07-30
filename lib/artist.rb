327088cbdb004dd03a256660cc84689489e9ab7b
Joseph Perez <hello@josephdanielperez.com>
2020-07-30 16:49:41 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,7 +24,8 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist_name)
-     self.all.find {|data| data.name == artist_name} || self.new(artist_name)
+   self.all.find {|data| data.name == artist_name} || self.new(artist_name)
+  end
 
   def print_songs
     self.songs.each do |song|
