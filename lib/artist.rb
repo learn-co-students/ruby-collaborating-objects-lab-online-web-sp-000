f0f14530a69a9c4ab4b7602563a5ebc9e5cc221b
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-05 01:10:16 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,11 +24,6 @@ class Artist
   
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
-    # if self.find(name)
-    #   self.find(name)
-    # else
-    #   self.create(name)
-    # end
   end
   
   def self.find_by_name(name)
@@ -37,8 +32,6 @@ class Artist
   
   def self.create_by_name(name)
     Artist.new(name)
-    # artist = self.new(name).save
-    # artist
   end
   
   def save 
