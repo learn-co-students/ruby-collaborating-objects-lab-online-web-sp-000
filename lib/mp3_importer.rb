8dd864b28e551a0e2b2503c489259bf1f2631b06
jasonronalddavis <jdavis@c.ringling.edu>
2020-08-24 17:15:17 -0500

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,16 @@
+class Mp3_importer
+
+attr_accessor :path
+
+
+def initialize(path)
+@path = path
+end
+
+def files
+
+end
+
+def Mp3_importer
+end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -20,11 +20,25 @@ end
 
 def artist_name=(name)
 
-    artisst = Artist.find_or_create_by_name(name)
+    artist = Artist.find_or_create_by_name(name)
 
     self.artist = artist
 
 end
+
+
+def self.new_by_filename(file)
+
+    row = file
+        data = row.split(" - ")
+          artist_name = data[0]
+          song_name = data[1].delete_suffix(".mp3").strip
+          song = Song.new(song_name)
+          song.artist_name = artist_name
+          song
+    end
+
+
 
     def artist= (artist)
         @artist = artist
