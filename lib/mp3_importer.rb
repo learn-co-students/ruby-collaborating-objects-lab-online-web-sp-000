3582fe3d0b9c8549cb4e8015332be252ad42dfa5
jasonronalddavis <jdavis@c.ringling.edu>
2020-08-24 18:50:14 -0500

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,9 +8,13 @@ def initialize(path)
 end
 
 def files
-
+    binding.pry
+    Dir.entries(path).select do |file|
+     file.size > 2 
+    end
 end
 
 def import
+    files.each { |file| Song.new_by_filename(file) }
 end
 end
