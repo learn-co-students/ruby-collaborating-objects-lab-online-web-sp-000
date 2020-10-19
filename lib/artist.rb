2ce0ddfa96780935d19fb39edee5d50420a5d9fa
Chris Houghton <chrishoughton2434@gmail.com>
2020-10-19 20:22:42 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,38 @@
+require "pry"
+
+class Artist
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.all.detect{|artist| artist.name == name} || self.new(name)
+  end
+
+  def print_songs
+    Song.all.map do |song|
+      if song.artist == self
+        puts song.name
+      end
+    end
+  end
+  
+end
