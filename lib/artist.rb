208af7781ae3a951ba8ebb35c9b055f53fd2b20a
Zachary Durr <zdurr22@gmail.com>
2020-11-10 22:40:25 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def add_song(song)
+    Song.all << song
+  end
+
+  def songs
+    Song.all
+  end
+
+  def find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
+  end
+
+
+end
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,14 @@
+class Mp3Importer
+  attr_accessor :file_path
+  @@all = []
+
+
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
+  def files
+
+  end
+
+end
