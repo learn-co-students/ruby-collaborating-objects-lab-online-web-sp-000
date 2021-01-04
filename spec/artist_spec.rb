c80269480bec6e72313e6a701e517d54e727145a
Christopher Spagnesi <christopherspagnesi@gmail.com>
2021-01-04 19:10:08 +0000

    Done.

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -1,87 +1,45 @@
-describe 'Artist' do
+class Artist
+  attr_accessor :name, :songs
 
-  before(:example) {
-    Artist.class_variable_set(:@@all, [])
-  }
+  @@all = []
 
-
-  describe '#initialize with #name' do
-    it 'accepts a name for the artist' do
-      artist = Artist.new('Michael Jackson')
-      expect(artist.name).to eq('Michael Jackson')
-    end
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
   end
 
-  describe '#name=' do
-    it 'sets the artist name' do
-      artist = Artist.new('Michael Jackson')
-      artist.name = 'King of Pop'
-      expect(artist.name).to eq('King of Pop')
-    end
+  def self.all
+    @@all
   end
 
-  describe '.all' do
-    it 'returns all existing Artist instances' do
-      artist = Artist.new('Michael Jackson')
-      expect(Artist.all).to eq([artist])
-      prince = Artist.new('Prince')
-      expect(Artist.all).to eq([artist, prince])
-    end
+  def self.create(name)
+    artist = self.new(name)
+    artist.name = name
+    artist
   end
 
-  describe '#add_song' do
-    it 'keeps track of an artist\'s songs' do
-      artist = Artist.new('Michael Jackson')
-      song_one = Song.new("Rock With You")
-      song_two = Song.new("Smooth Criminal")
-      smells_like_teen_spirit = Song.new("Smells Like Teen Spirit")
-      artist.add_song(song_one)
-      artist.add_song(song_two)
-      expect(artist.songs).to eq([song_one, song_two])
-    end
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
   end
 
-  describe '#songs' do
-    it 'lists all songs that belong to this artist' do
-      artist = Artist.new('Michael Jackson')
-      dirty_diana = Song.new("Dirty Diana")
-      billie_jean = Song.new("Billie Jean")
-      piano_man = Song.new("Piano Man")
-      artist.add_song(dirty_diana)
-      artist.add_song(billie_jean)
-      expect(artist.songs).to eq([dirty_diana, billie_jean])
-    end
+  def self.find_or_create_by_name(name)
+    self.find(name) || self.create(name)
   end
 
-  describe '.find_or_create_by_name' do
-    it 'always returns an Artist instance' do
-      artist_1 = Artist.find_or_create_by_name("Michael Jackson")
-      artist_2 = Artist.find_or_create_by_name("Michael Jackson")
-      expect(artist_1).to be_an(Artist)
-      expect(artist_2).to be_an(Artist)
-    end
-
-    it 'finds or creates an artist by name maintaining uniqueness of objects by name property' do
-      artist_1 = Artist.find_or_create_by_name("Michael Jackson")
-      artist_2 = Artist.find_or_create_by_name("Michael Jackson")
-      expect(artist_1).to eq(artist_2)
-    end
-
-    it 'Creates new instance of Artist if none exist' do
-      artist_1 = Artist.find_or_create_by_name("Drake")
-      expect(artist_1.class).to eq(Artist)
-    end
+  def add_song(song)
+    @songs << song
   end
 
-  describe '#print_songs' do
-    it 'lists all of the artist\'s songs' do
-      artist = Artist.new('Michael Jackson')
-      dirty_diana = Song.new("Dirty Diana")
-      billie_jean = Song.new("Billie Jean")
-      piano_man = Song.new("Piano Man")
-      artist.add_song(dirty_diana)
-      artist.add_song(billie_jean)
-      expect{artist.print_songs}.to output("Dirty Diana\nBillie Jean\n").to_stdout
-    end
+  def print_songs
+    songs.each { |song| puts song.name }
+  end
+
+  def save
+    @@all << self
+  end
+
+  def songs
+    @songs
   end
 end
