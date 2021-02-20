d779c12ef48094ba36bb2bc8b990b107f7b5e0b5
HELEN HARMAN <hlharman@bellsouth.net>
2021-02-20 02:17:10 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,23 +10,25 @@ class Song
   end
   
    def save 
-    @@all<<self
+    @@all << self
   end 
   
   def self.all 
     @@all 
   end 
   
-  def self.new_by_filename(filename)
-     song = self.new(name)
-    song.name = filename.split(" - ")[1]
-    song.artist 
-  end 
+  def self.new_by_filename(file_name)
+    #pry(Song)> filename => "Michael Jackson - Black or White - pop.mp3"
+    #pry(Song)> filename.split(" - ")[1] => "Black or White"
+    song_name = file_name.split(" - ")[1] 
+    artist_one = file_name.split(" - ")[0]
+    new_song = Song.new(song_name)
+    new_song.artist_name=(artist_one)
+    new_song
+  end
   
-  def artist_name=artist 
-    if self.artist
-    self.artist.name
-  end 
+  def artist_name=(name) 
+      self.artist = Artist.find_or_create_by_name(name)
   end 
 end
 
