f2764caf416f537f41f3b5c8753bbb08c8a3016d
minirachel <rachel.a.levinson@gmail.com>
2021-02-24 16:59:14 -0500

    Done.

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -12,11 +12,13 @@ class MP3Importer
 
     def files
         # Dir.entries(path).glob('*.mp3')
-        all_file_names = Dir.entries(path)
-        all_file_names.grep(/.mp3/)
+        all_file_names = Dir.entries(path).grep(/.mp3/)
     end
 
     def import
+        files.each do |filename|
+            Song.new_by_filename(filename)
+        end
 
     end
 
--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,6 @@ class Song
 
     def initialize(name)
         @name = name
-        @artist = artist
         @@all << self
     end
 
@@ -20,7 +19,7 @@ class Song
 
     def self.new_by_filename(file)
         # DONE #parse filename 
-        #create a new song instance using string from parsed filename
+        #DONE create a new song instance using string from parsed filename
         #associate new song with the artist
         #return new song instance
         #Michael Jackson - Black or White - pop.mp3
@@ -28,12 +27,17 @@ class Song
 
         file_name = file.chomp('.mp3')
         file_info = file_name.split(' - ')
-        self.new(file_info[1])
-        self.artist = file_info[0]
+        #["Michael Jackson", "Black or White", "pop"]
+        new_song = self.new(file_info[1])
+        new_song.artist_name=(file_info[0])
 
+        new_song
     end
 
-#     def artist_name=
-#         #turn artists name from string TO Artist object
-#     end
+    def artist_name=(name)
+        #turn artists name from string TO Artist object
+        self.artist = Artist.find_or_create_by_name(name)
+        artist.add_song(self)
+    end
+
 end
