c8b6af3740c01799904cdc68bc5fa11ef711884d
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-27 15:32:01 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,11 +16,15 @@ class Artist
     @songs << song
   end
 
-  # def save
-  #   @@all << self
-  # end
+  def save
+    @@all << self
+  end
 
   def self.find_or_create_by_name(name)
+    self.all.detect { |artist| artist.name == name } || Artist.new(name)
+  end
 
+  def print_songs
+    @songs.each { |song| puts song.name }
   end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,28 @@
+require 'pry'
+
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
+
+  @@all = []
 
   def initialize(name)
     @name = name
-    # @@all << self
+    # @artist = artist
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+    # binding.pry
+    split_name = filename.split(" - ")
+    song = self.new(split_name[1])
+    artist = Artist.find_or_create_by_name(split_name[0])
+    song.artist = artist
+    artist.add_song(song)
+    song
   end
 
 
