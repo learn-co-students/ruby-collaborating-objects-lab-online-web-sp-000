e943816a63c2ad26a9d7930fd6aa2800599e421f
dotnotation <dotinmotionproductions@gmail.com>
2021-05-25 19:28:41 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,47 @@
 class Artist
+    #create artist or finding the artist
+    attr_accessor :name, :song
 
-    attr_accessor :name
-  
     @@all = []
-  
+    
     def initialize(name)
-      @name = name
-      @@all << self
+        @name = name
+        @@all << self
     end
-  
+
+    def save
+        @@all << self
+    end
+
     def self.all
-      @@all
+        @@all
     end
-  
-    def add_song(song)
-      song.artist = self
-    end
-  
+
     def songs
-      Song.all.select {|song| song.artist == self}
+        Song.all.select {|song| song.artist == self}
     end
-  
+
+    def add_song(song)
+        song.artist= self
+    end
+
+    def self.create_by_name(name)
+        Artist.new(name)
+        # artist = self.new(name)
+        # artist.name = name
+        # artist.save
+        # artist
+    end
+
+    def self.find_by_name(name)
+        self.all.find {|artist| artist.name == name}
+    end
+
     def self.find_or_create_by_name(name)
-      self.all.find {|artist| artist.name == name} || Artist.new(name)
+        self.find_by_name(name) || self.create_by_name(name)
     end
-  
+
     def print_songs
-      self.songs.each {|song| puts song.name}
+        self.songs.each {|song| puts song.name}
     end
-  
-  end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,32 @@
+require 'pry'
 class Song
+    #create songs given each filename and sending the artist name to Artist
+    #relies on Artist
     attr_accessor :name, :artist
-  
+
     @@all = []
-  
+
     def initialize(name)
-      @name = name
-      @@all << self
+        @name = name
+        @@all << self
     end
-  
+
     def self.all
-      @@all
+        @@all
     end
-  
+
     def self.new_by_filename(filename)
-      splitname = filename.split(" - ")
-      song = self.new(splitname[1])
-      artist = Artist.find_or_create_by_name(splitname[0])
-      song.artist = artist
-      artist.add_song(song)
-      song
+        song_name = filename.split(" - ")[1]
+        artist_name = filename.split(" - ")[0]
+        new_song = Song.new(song_name)
+        new_song.artist = Artist.new(artist_name)
+        new_song
     end
-  
-    def artist_name=(name)
-      self.artist = Artist.find_or_create_by_name(name)
-      artist.add_song(self)
+
+    def artist_name=(name) #this doesn't pass it says the array returns 2 instead of 1 so the item is being duplicated
+        self.artist = Artist.find_or_create_by_name(name) 
+        artist.add_song(self)
+        #binding.pry
     end
-  end
+
+end
