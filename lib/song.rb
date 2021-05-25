db27f754d74ec7a1f95fd950c4d631414818dae1
dotnotation <dotinmotionproductions@gmail.com>
2021-05-25 15:38:40 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -21,7 +21,7 @@ class Song
 
     def self.new_by_filename(filename)
         artist_song = filename.split(" - ")
-        song = self.new(artist_song)[1]
+        song = self.new(artist_song[1])
         artist = Artist.find_or_create_by_name(artist_song[0])
         song.artist = artist
         artist.add_song(song)
